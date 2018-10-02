4eb63922f0dc97ab26e16be8112b11ea09ca4069
Rama Roberto <bertoski@gmail.com>
2018-10-02 11:06:03 -0700

    Adds changes of run.py

--- examples/inverted-single-pole/run.py
+++ examples/inverted-single-pole/run.py
@@ -6,6 +6,7 @@ import gym
 import math
 import numpy as np
 import time
+import datetime
 
 from cartpole import CartPoleEnv
 from copy import deepcopy
@@ -22,11 +23,6 @@ local_neat = imp.load_module("neat", f, pathname, desc)
 
 import neat
 
-# Config for FeedForwardNetwork.
-config = neat.config.Config(neat.genome.DefaultGenome, neat.reproduction.DefaultReproduction,
-                            neat.species.DefaultSpeciesSet, neat.stagnation.DefaultStagnation,
-                            'config_inverted_single_pole')
-
 num_workers = 6
 env = CartPoleEnv()
 envs_pool = [CartPoleEnv() for i in range(num_workers*2)]
@@ -42,7 +38,7 @@ def ini_pop(state, stats, config, output):
     pop.add_reporter(stats)
     return pop
 
-def eval_genome(genome, config, visualize=False):
+def eval_genome(genome, config, visualize=False, normalize_input=False):
     
     # Get a cartpole's environment from the pool
     local_env = envs_pool.pop(0)
@@ -55,10 +51,25 @@ def eval_genome(genome, config, visualize=False):
         for j in xrange(200): # 5 seconds of total time: 5 / 0.025 (ts) = 200
             if visualize:
                 local_env.render()
-            # +/-3, +/-5?, +/-2pi, +/-2pi
+                if j == 0:
+                    time.sleep(1.0)
+                else:
+                    time.sleep(0.015)
+            
+            out = None
             x, x_dot, theta, theta_dot = obs
-            out = net.activate([x/4.0, x_dot/10.0, math.cos(theta), math.sin(theta), theta_dot/math.pi])
-            force = max(-10, min(10, out[0] * 20 - 10))
+            if normalize_input:
+                # +/-3, +/-10, +/-2pi, +/-2pi
+                inp = [1, x/3.0, math.cos(theta), math.sin(theta), x_dot/10.0, theta_dot/2*math.pi]
+            else:
+                # out = net.activate([1, x, x_dot, math.cos(theta), math.sin(theta), theta_dot])
+                # out = net.activate([1, x, math.cos(theta), math.sin(theta), x_dot, theta_dot])
+                inp = [1, x, math.cos(theta), math.sin(theta), x_dot, theta_dot]
+            
+            inp.reverse()    
+            out = net.activate(inp)
+            force = out[0] * 10
+            
             obs, reward, done, info = local_env.step(force)
             reward_streak += reward
             if done:
@@ -75,41 +86,72 @@ def eval_genome(genome, config, visualize=False):
 
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
-        genome.fitness = eval_genome(genome, config)
+        genome.fitness = eval_genome(genome, config, normalize_input=True)
 
-def run_neat(gens, env, config, max_trials=100, output=True):
+def run_neat(gens, env, config, output=True):
     pop = neat.Population(config)
     stats = neat.StatisticsReporter()
     pop.add_reporter(stats)
-    pop.add_reporter(neat.StdOutReporter(True))
+    pop.add_reporter(neat.StdOutReporter(output))
 
     pe = neat.ParallelEvaluator(num_workers, eval_genome)
     winner = pop.run(pe.evaluate, gens)
     
-    ran_generations = len(stats.generation_statistics)-1
+    ran_generations = len(stats.generation_statistics) - 1
     
     return winner, stats, ran_generations
     
-def run_experiment():
+def run_experiment(config_name, repetitions=1, max_generations=200):
+    # Config for FeedForwardNetwork.
+    config = neat.config.Config(neat.genome.DefaultGenome, neat.reproduction.DefaultReproduction,
+                                neat.species.DefaultSpeciesSet, neat.stagnation.DefaultStagnation,
+                                config_name)
+                                
     # Setup logger and environment.
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     
     # env = gym.make("CartPole-v1")
+    
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    
+    f = open(timestamp+"_"+config_name+".log", "wa", 0) # buffer size = 0
 
     # Run!
     results = []
-    n_experiments = 1
-    for i in xrange(n_experiments):
-        winner, stats, generations = run_neat(500, env, config)
+    experiments_generations = []
+    for i in xrange(repetitions):
+        winner, stats, generations = run_neat(max_generations, env, config)
         results.append((winner, stats, generations))
-        print "Experiment " + (str(i+1)) + "/" + str(n_experiments) + " finished."
-        time.sleep(2)
+        experiments_generations.append(generations)
+        msg = "Experiment " + (str(i+1)) + "/" + str(repetitions) + " finished.\n"
+        msg += "Generations: " + str(generations) + "\n"
+        msg += "Generations percentiles: " + \
+            str(np.percentile(experiments_generations, 10)) + " " + \
+            str(np.percentile(experiments_generations, 25)) + " " + \
+            str(np.percentile(experiments_generations, 50)) + " " + \
+            str(np.percentile(experiments_generations, 75)) + " " + \
+            str(np.percentile(experiments_generations, 90)) + "\n"
+        msg += "Generations average/std: " + \
+            str(np.mean(experiments_generations)) + " " + \
+            str(np.std(experiments_generations)) + "\n\n"
+
+        f.write(msg)
+        f.flush()
+        print msg
+    
+    f.close()
+        
     
     # Save results to file
-    if n_experiments > 1:
-        f = open("experiment.data", "wb")
+    if repetitions > 1:
+        f = open(timestamp+"_"+config_name+".data", "wb")
         pickle.dump(results, f)
+        return [np.percentile(experiments_generations, 10),\
+            np.percentile(experiments_generations, 25),\
+            np.percentile(experiments_generations, 50),\
+            np.percentile(experiments_generations, 75),\
+            np.percentile(experiments_generations, 90)]
     else:
         # Visualize
         reward = eval_genome(winner, config, visualize=True)
@@ -124,13 +166,60 @@ def load_experiment():
     winner, stats, generations =  results[0]
     
     # Visualize
-    reward = eval_genome(winner, config, visualize=True)
+    reward = eval_genome(winner, config, visualize=False)
     print "Total reward is:", reward, eval_genome(winner, config)
+
+from random import shuffle
+
+def load_manual_nn():
+    # bias, x, cos, sin, x_dot, theta_dot
+    input_to_output_conns_weights = [-1.27081788905737, -2.16328518046126, -1.17322972615653,  2.05416948816017, -4.17464738664070, -1.92563440012328]
+    
+    genome = neat.DefaultGenome(0)
+    genome.configure_new(config.genome_config)
+    connections = sorted(genome.connections.values(), key=lambda conn: conn.key[0])
+    
+    # Set the weights of the out connections
+    for i, ck in enumerate(sorted(genome.connections.keys(), key=lambda k: -k[0])):
+        genome.connections[ck].set("weight", input_to_output_conns_weights[i])
+    reward = eval_genome(genome, config)
+    
+    # Add the new node
+    genome.add_node(config.genome_config, connections[-1])
+    connections[-1].enabled = True # Re-enable the connection
+    
+    # Define the 1 to out connection's weight
+    genome.connections[(1, 0)].weight = -0.21993945865389
+    
+    # Add the remaining connections
+    genome.connections[(-1, 1)].weight = 0.818966241451037 # bias to 1
+    genome.add_connection(config.genome_config, -2, 1, 4.45185524142824, True) # x to 1
+    genome.add_connection(config.genome_config, -4, 1, -2.06666066002367, True) # sin to 1
+    genome.add_connection(config.genome_config, -6, 1, 0.180692888372585, True) # theta_dot to 1
+    
+    print genome
+    print eval_genome(genome, config, visualize=True)
+    # print "Total reward is:", reward, eval_genome(genome, config)
 
 # If run as script.
 if __name__ == '__main__':
     
-    run_experiment()
+    # load_manual_nn()
+    # run_experiment("config_inverted_single_pole", repetitions=50, max_generations=500)
+    
+    # run_experiment("config_inverted_single_pole_stag_20_st_03_ctm", repetitions=200, max_generations=500)
+    run_experiment("config_inverted_single_pole_stag_20_st_03", repetitions=200, max_generations=500)
+    run_experiment("config_inverted_single_pole_stag_20_st_03_wmp", repetitions=200, max_generations=500)
+    
+    # run_experiment("config_inverted_single_pole_stag_20_st_03_atfb", repetitions=100, max_generations=500)
+    # run_experiment("config_inverted_single_pole_stag_20_st_03_wmp", repetitions=100, max_generations=500)
+    # run_experiment("config_inverted_single_pole_stag_20_st_03_cp_05", repetitions=100, max_generations=500)
+    # run_experiment("config_inverted_single_pole_stag_20_st_03_tn_6", repetitions=100, max_generations=500)
+    # run_experiment("config_inverted_single_pole_stag_20_st_03_structural_probs_reduced", repetitions=100, max_generations=500)
+    # run_experiment("config_inverted_single_pole_stag_20_st_03_no_elitism", repetitions=100, max_generations=500)
+    # run_experiment("config_inverted_single_pole_stag_15_wmp_05", repetitions=100, max_generations=500)
+    # run_experiment("config_inverted_single_pole_stag_15", repetitions=50, max_generations=500)
+    # run_experiment("config_inverted_single_pole_stag_15_structural_mut_changed", repetitions=50, max_generations=500)
     
     # load_experiment()
     
@@ -140,3 +229,11 @@ if __name__ == '__main__':
     # with open('neat_pole_balancing_winner.pkl', 'wb') as output:
     #     pickle.dump(winner_net, output, pickle.HIGHEST_PROTOCOL)
 
+# 0.818966241451037    -1.27081788905737    bias
+# 4.45185524142824	   -2.16328518046126    x
+# 0	                   -1.17322972615653    cos
+# -2.06666066002367	    2.05416948816017    sin
+# 0	                   -4.17464738664070    x_dot
+# 0.180692888372585	   -1.92563440012328    theta_dot
+# 0	                   -0.21993945865389    node 7 to out
+# 0	                    0
